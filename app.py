8dc8f50277ae0647aabfe1a35da20437b85f799f
Nikhil S Thorat <nikhilsthorat03@gmail.com>
2025-01-18 20:11:34 -0800

    llllppppp

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    print("Hello CI-CD successfully .")
+    print("Hello CI-CD successfully.")
     return {
         'statusCode': 200,
         'body': 'Hello World GG You are great! Finally we did it....'
